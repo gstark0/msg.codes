632bdbcdda126eb5025c701fb06435e5ebb3a753
Greg Stark <gstark@Gregs-MacBook-Pro.local>
2019-11-20 20:28:52 +0100

    POST new link data to Flask

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+
+db_name = 'database.db'
 
 app = Flask(__name__)
+
+@app.route('/link', methods=['POST'])
+def add_link():
+    data = request.get_json()
+
+    return 'ok'
 
 @app.route('/')
 def main():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- static/js/generate.js
+++ static/js/generate.js
@@ -7,6 +7,19 @@ function generateLink() {
     if(name && email) {
         let btn = document.body.querySelector('#generate-button');
         btn.style.backgroundColor = '#FA9E8C';
+
+        // Generate new link
+        fetch('/link', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name: name, email: email})
+        }).then(resp => {
+            console.log(resp)
+        });
+
+        // Change button's text to a new link
         btn.innerHTML = 'msg.codes/y1Dz1g';
     } else {
         alert('not works');
